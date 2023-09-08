2ae1d4e8fd7b8d295b12d73d55f25e709bbce355
Julien Dessaux <julien.dessaux@adyxax.org>
2023-09-08 22:34:22 +0200

    [javascript] add database function to fetch all contracts

--- nodejs/database/contracts.js
+++ nodejs/database/contracts.js
@@ -2,6 +2,7 @@ import db from './db.js';
 
 const addContractStatement = db.prepare(`INSERT INTO contracts(data) VALUES (json(?));`);
 const getContractStatement = db.prepare(`SELECT data FROM contracts WHERE data->>'id' = ?;`);
+const getContractsStatement = db.prepare(`SELECT data FROM contracts WHERE data->>'fulfilled' = false;`);
 const updateContractStatement = db.prepare(`UPDATE contracts SET data = json(:data) WHERE data->>'id' = :id;`);
 
 export function getContract(id) {
@@ -10,6 +11,11 @@ export function getContract(id) {
 		return null;
 	}
 	return JSON.parse(data.data);
+}
+
+export function getContracts() {
+	const data = getContractsStatement.all();
+	return data.map(contractData => JSON.parse(contractData.data));
 }
 
 export function setContract(data) {
