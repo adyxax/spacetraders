4dd8a2e90f3cc7a2534af8fe7da6bd6e31ef6c28
Julien Dessaux <julien.dessaux@adyxax.org>
2023-10-18 22:58:23 +0200

    [haskell] add missing indices

--- haskell/src/SpaceTraders/Database/000_init.sql
+++ haskell/src/SpaceTraders/Database/000_init.sql
@@ -13,11 +13,14 @@ CREATE TABLE contracts (
   id INTEGER PRIMARY KEY,
   data TEXT NOT NULL
 );
+CREATE UNIQUE INDEX contracts_data_id ON contracts (json_extract(data, '$.id'));
+CREATE UNIQUE INDEX contracts_data_fulfilled ON contracts (json_extract(data, '$.fulfilled'));
 CREATE TABLE ships (
   id INTEGER PRIMARY KEY,
   data TEXT NOT NULL
 );
 CREATE UNIQUE INDEX ships_data_symbol ON ships (json_extract(data, '$.symbol'));
+CREATE INDEX ships_data_nav_systemSymbol ON ships (json_extract(data, '$.nav.systemSymbol'));
 CREATE TABLE systems (
   id INTEGER PRIMARY KEY,
   data TEXT NOT NULL
